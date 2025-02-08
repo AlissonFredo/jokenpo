1a2bc40277dca9ecc17997a28346f5532ca4276e
AlissonFredo <douglasalissonsf@gmail.com>
2025-02-07 21:13:29 -0300

    feat: adds Play page
    Creates the Play page, responsible for starting the game.

---
+++ src/pages/play/index.jsx
@@ -0,0 +1,12 @@
+import { Link } from "react-router-dom";
+
+function Play() {
+  return (
+    <div>
+      <h1>Jokenpo</h1>
+      <Link to="/game">play</Link>
+    </div>
+  );
+}
+
+export default Play;
--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -1,10 +1,11 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Play from "../pages/play";
 
 function Routers() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<h1>play</h1>} />
+        <Route path="/" element={<Play />} />
         <Route path="/game" element={<h1>game</h1>} />
       </Routes>
     </BrowserRouter>
