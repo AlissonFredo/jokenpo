14ad6465b9ffab439249e103ff1d192c18e49503
AlissonFredo <douglasalissonsf@gmail.com>
2025-02-08 16:02:22 -0300

    feat: adds play function to the game
    Adds the play function, responsible for:
    
    - Receive the option chosen by the player.
    
    - Draw the computer option.
    
    - Determine the winner based on the choices.

--- src/pages/game/index.jsx
+++ src/pages/game/index.jsx
@@ -1,12 +1,57 @@
+import { useState } from "react";
+
 function Game() {
+  const options = ["rock", "paper", "scissor"];
+
+  const [resultado, setResultado] = useState("");
+
+  const sorteaOpcaoComputador = () => {
+    return options[Math.floor(Math.random() * options.length)];
+  };
+
+  const play = (optionPlayer) => {
+    const opcaoComputador = sorteaOpcaoComputador();
+
+    console.log("Player", optionPlayer);
+    console.log("Computador", opcaoComputador);
+
+    if (optionPlayer == opcaoComputador) {
+      console.log("Resultado", "DRAW GAME");
+
+      setResultado("DRAW GAME");
+    } else if (optionPlayer == "rock" && opcaoComputador == "scissor") {
+      console.log("Resultado", "YOU WIN!");
+
+      setResultado("YOU WIN!");
+    } else if (optionPlayer == "scissor" && opcaoComputador == "rock") {
+      console.log("Resultado", "YOU WIN!");
+
+      setResultado("YOU WIN!");
+    } else if (optionPlayer == "scissor" && opcaoComputador == "paper") {
+      console.log("Resultado", "YOU WIN!");
+
+      setResultado("YOU WIN!");
+    } else {
+      console.log("Resultado", "YOU LOSE!");
+      setResultado("YOU LOSE!");
+    }
+
+    setTimeout(() => {
+      setResultado("");
+    }, 1000);
+  };
+
   return (
     <div>
       <div style={{ display: "flex", justifyContent: "space-between" }}>
-        <div style={{ display: "inline-block" }}>
+        <div>
           <p>player</p>
           <p>0</p>
         </div>
-        <div style={{ display: "inline-block" }}>
+        <div>
+          <p>{resultado}</p>
+        </div>
+        <div>
           <p>computador</p>
           <p>0</p>
         </div>
@@ -29,9 +74,9 @@ function Game() {
       </div>
 
       <div style={{ display: "flex", justifyContent: "center" }}>
-        <button>&#9994; pedra</button>
-        <button>&#9995; papel</button>
-        <button>&#9996; tesoura</button>
+        <button onClick={() => play(options[0])}>&#9994; pedra</button>
+        <button onClick={() => play(options[1])}>&#9995; papel</button>
+        <button onClick={() => play(options[2])}>&#9996; tesoura</button>
       </div>
     </div>
   );
